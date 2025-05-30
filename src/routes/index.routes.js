7c5bb07b49c2fb79889089435add789806f4c343
goDev <nguyentrongtiendev06@gmail.com>
2025-05-31 05:47:32 +0700

    Xóa toàn bộ code trong branch main

--- src/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import TaskRoute from "./task.route.js";
-import AuthRoute from "./auth.routes.js";
-const route = express.Router();
-
-route.use("/tasks", TaskRoute);
-route.use("/users", AuthRoute);
-
-export default route;
