d4aa27f969f7d094702c6994dad62a88feec2194
buiquocbinh811 <binhbinhbinh8112004@gmail.com>
2025-06-04 08:43:00 +0700

    delete redundant files

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,13 +1,11 @@
 import express from "express";
 import AuthRoute from "./auth.routes.js";
-import RoleRoute from "./role.routes.js";
 import PermissionRoute from "./permission.routes.js";
 import RolePermissionRoute from "./rolePermission.routes.js";
 
 const route = express.Router();
 
 route.use("/users", AuthRoute);
-route.use("/roles", RoleRoute);
 route.use("/permissions", PermissionRoute);
 route.use("/role-permissions", RolePermissionRoute);
 
